1e07e23d4b6524628800e7baddb3bf8407ebafb7
Ajayyy-code <79764618+Ajayyy-code@users.noreply.github.com>
2021-10-31 15:54:21 -0500

    Edited JavaScript File

---
+++ app.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    var search = document.querySelector("button");
+    var resultArea = document.querySelector(".result");
+    var query = document.querySelector("input");
+    search.addEventListener('click', handleClick);
+    var httpRequest = new XMLHttpRequest();
+    
+    function handleClick(clickEvent){
+        clickEvent.preventDefault();
+        //console.log("I GOT CLICKED")
+        var url = "superheroes.php?query=" + query.value;
+        httpRequest.onreadystatechange = fetchingdata;
+        httpRequest.open('GET', url, true);
+        httpRequest.send();
+    }
+    function fetchingdata(){
+        if (httpRequest.readyState === XMLHttpRequest.DONE){
+            if (httpRequest.status === 200){
+                var response = httpRequest.responseText;
+                resultArea.innerHTML = response;
+            }
+            else{
+                resultArea.innerHTML = "Error: This resquest can not be deliver. Please try again.";
+            }
+        }
+    }
+}
