a7232a03c3f06e94bc25844e8e6988569474f829
Ajayyy-code <79764618+Ajayyy-code@users.noreply.github.com>
2021-11-03 21:50:40 -0500

    Fixed a Bug

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 window.onload = function(){
     var search = document.querySelector("button");
-    var resultArea = document.querySelector(".result");
+    var result = document.querySelector(".result");
     var query = document.querySelector("input");
     search.addEventListener('click', handleClick);
     var httpRequest = new XMLHttpRequest();
     
     function handleClick(clickEvent){
         clickEvent.preventDefault();
-        //console.log("I GOT CLICKED")
         var url = "superheroes.php?query=" + query.value;
         httpRequest.onreadystatechange = fetchingdata;
         httpRequest.open('GET', url, true);
@@ -17,10 +16,10 @@ window.onload = function(){
         if (httpRequest.readyState === XMLHttpRequest.DONE){
             if (httpRequest.status === 200){
                 var response = httpRequest.responseText;
-                resultArea.innerHTML = response;
+                result.innerHTML = response;
             }
             else{
-                resultArea.innerHTML = "Error: This resquest can not be deliver. Please try again.";
+                result.innerHTML = "Error: This resquest can not be deliver. Please try again.";
             }
         }
     }
